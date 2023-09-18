8233f06fef371b90fc9105a7bd035d94f231604f
tuandung0990 <142638808+tuandung0990@users.noreply.github.com>
2023-09-18 10:27:26 +0700

    Update face_enhancer.py

--- roop/processors/frame/face_enhancer.py
+++ roop/processors/frame/face_enhancer.py
@@ -39,8 +39,82 @@ def get_face_enhancer() -> Any:
             FACE_ENHANCER = gfpgan.GFPGANer(model_path=model_path, upscale=1) # type: ignore[attr-defined]
     return FACE_ENHANCER
 
+def norm_crop2(img, landmark, image_size, enable_padding=True):
+	lm = numpy.array(landmark)
+	eye_left = lm[0]
+	eye_right = lm[1]
+	mouth_avg = (lm[3] + lm[4]) * 0.5
 
-def enhance_face(temp_frame: Frame) -> Frame:
+	eye_avg = (eye_left + eye_right) * 0.5
+	eye_to_eye = eye_right - eye_left
+	eye_to_mouth = mouth_avg - eye_avg
+
+	x = eye_to_eye - numpy.flipud(eye_to_mouth) * [-1, 1]
+	x /= numpy.hypot(*x)
+	rect_scale = 1
+	x *= max(numpy.hypot(*eye_to_eye) * 2.0 * rect_scale, numpy.hypot(*eye_to_mouth) * 1.8 * rect_scale)
+	y = numpy.flipud(x) * [-1, 1]
+	c = eye_avg + eye_to_mouth * 0.1
+	quad = numpy.stack([c - x - y, c - x + y, c + x + y, c + x - y])
+	qsize = numpy.hypot(*x) * 2
+
+	quad_ori = numpy.copy(quad)
+	shrink = int(numpy.floor(qsize / image_size * 0.5))
+	if shrink > 1:
+		h, w = img.shape[0:2]
+		rsize = (int(numpy.rint(float(w) / shrink)), int(numpy.rint(float(h) / shrink)))
+		img = cv2.resize(img, rsize, interpolation=cv2.INTER_AREA)
+		quad /= shrink
+		qsize /= shrink
+
+	h, w = img.shape[0:2]
+	border = max(int(numpy.rint(qsize * 0.1)), 3)
+	crop = (int(numpy.floor(min(quad[:, 0]))), int(numpy.floor(min(quad[:, 1]))), int(numpy.ceil(max(quad[:, 0]))),
+			int(numpy.ceil(max(quad[:, 1]))))
+	crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, w), min(crop[3] + border, h))
+	if crop[2] - crop[0] < w or crop[3] - crop[1] < h:
+		img = img[crop[1]:crop[3], crop[0]:crop[2], :]
+		quad -= crop[0:2]
+
+	h, w = img.shape[0:2]
+	pad = (int(numpy.floor(min(quad[:, 0]))), int(numpy.floor(min(quad[:, 1]))), int(numpy.ceil(max(quad[:, 0]))),
+		   int(numpy.ceil(max(quad[:, 1]))))
+	pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - w + border, 0), max(pad[3] - h + border, 0))
+	if enable_padding and max(pad) > border - 4:
+		pad = numpy.maximum(pad, int(numpy.rint(qsize * 0.3)))
+		img = numpy.pad(img, ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+		h, w = img.shape[0:2]
+		y, x, _ = numpy.ogrid[:h, :w, :1]
+		mask = numpy.maximum(1.0 - numpy.minimum(numpy.float32(x) / pad[0],
+										   numpy.float32(w - 1 - x) / pad[2]),
+						  1.0 - numpy.minimum(numpy.float32(y) / pad[1],
+										   numpy.float32(h - 1 - y) / pad[3]))
+		blur = int(qsize * 0.02)
+		if blur % 2 == 0:
+			blur += 1
+		blur_img = cv2.boxFilter(img, 0, ksize=(blur, blur))
+
+		img = img.astype('float32')
+		img += (blur_img - img) * numpy.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+		img += (numpy.median(img, axis=(0, 1)) - img) * numpy.clip(mask, 0.0, 1.0)
+		img = numpy.clip(img, 0, 255)  # float32, [0, 255]
+		quad += pad[:2]
+
+	dst_h, dst_w = image_size, image_size
+	template = numpy.array([[0, 0], [0, dst_h], [dst_w, dst_h], [dst_w, 0]])
+	affine_matrix = cv2.estimateAffinePartial2D(quad, template, method=cv2.LMEDS)[0]
+	cropped_face = cv2.warpAffine(img, affine_matrix, (dst_w, dst_h), borderMode=cv2.BORDER_CONSTANT, borderValue=(135, 133, 132))  # gray
+	affine_matrix = cv2.estimateAffinePartial2D(quad_ori, numpy.array([[0, 0], [0, image_size], [dst_w, dst_h], [dst_w, 0]]), method=cv2.LMEDS)[0]
+
+	return cropped_face, affine_matrix
+    
+def enhance_face(target_face: Face, temp_frame: Frame) -> Frame:
+	face_enhancer = get_face_enhancer()
+	face_size = 512
+	temp_face, matrix = norm_crop2(temp_frame, target_face['kps'], face_size)
+	temp_face = temp_face.astype(numpy.float32)[:,:,::-1] / 255.0
+	temp_face = (temp_face - 0.5) / 0.5
+	temp_face = numpy.expand_dims(temp_face.transpose(2, 0, 1), axis = 0).astype(numpy.float32)
     with THREAD_SEMAPHORE:
         _, _, temp_frame = get_face_enhancer().enhance(
             temp_frame,
